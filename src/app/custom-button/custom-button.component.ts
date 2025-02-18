5222a5ff1cd73cde80aabafd66a93264e41daba6
EricBO2 <eric.carlsson@stud.sti.se>
2025-02-18 10:49:13 +0100

    added functions and buttons that subtrakt and add to a count

--- src/app/custom-button/custom-button.component.ts
+++ src/app/custom-button/custom-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output  } from '@angular/core';
 
 @Component({
   selector: 'app-custom-button',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './custom-button.component.css'
 })
 export class CustomButtonComponent {
-
+  @Input() buttonMessage!: string;
+  @Output() clicked = new EventEmitter<void>();
+  handledClik() {
+    this.clicked.emit();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { TextBoxComponent } from "../text-box/text-box.component";
+import { TextBoxComponent } from '../text-box/text-box.component';
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
+import { AboutComponent } from '../about/about.component';
+import { generate } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [TextBoxComponent],
+  imports: [TextBoxComponent, CustomButtonComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  count: number=0;
+  
+  add(){
+    this.count=addToNumber(this.count)
+  }
+  subtract(){
+    this.count=subtractToNumber(this.count)
+  }
 
+}
+
+function addToNumber(count:number):number{
+  return count+1;
+}
+function subtractToNumber(count:number):number{
+  return count-1;
 }
